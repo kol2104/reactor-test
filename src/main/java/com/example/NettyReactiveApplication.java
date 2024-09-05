c0928846c508aae2428ba477635dd5c08d3bb5f8
kol2104 <kni2104kni@mail.ru>
2024-09-05 17:08:19 +0500

    Add scheduled task

--- src/main/java/com/example/NettyReactiveApplication.java
+++ src/main/java/com/example/NettyReactiveApplication.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
+@EnableScheduling
 public class NettyReactiveApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/controller/MyWebSocketHandler.java
+++ src/main/java/com/example/controller/MyWebSocketHandler.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
@@ -8,12 +9,18 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MyWebSocketHandler implements WebSocketHandler {
 
+    public static List<WebSocketSession> sessionList = new ArrayList<>();
+
     @Override
     public Mono<Void> handle(WebSocketSession session) {
+        sessionList.add(session);
         return session.receive()
                 .map(WebSocketMessage::getPayloadAsText)
                 .flatMap(message -> {
@@ -23,5 +30,15 @@ public class MyWebSocketHandler implements WebSocketHandler {
                             .map(tick -> session.textMessage("Reversed: " + reversedMessage + " - " + tick));
                     return session.send(responseFlux);
                 }).then();
+    }
+
+    @Scheduled(fixedDelay = 2000L)
+    public void sched() {
+        System.out.println("Sched");
+        for (WebSocketSession session : sessionList) {
+            System.out.println(session.getId());
+            Mono<WebSocketMessage> responseMono = Mono.just(session.textMessage("Session id: " + session.getId() + " Local: " + LocalDateTime.now()));
+            session.send(responseMono).block();
+        }
     }
 }
